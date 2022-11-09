346440b540a7f82f2ffd236f3cf0767be66dd170
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-09 06:44:09 +0530

    Polyfill for reduce()

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
-//Polyfill for Map
+//Polyfill for Reduce
 
+//Initial value can be provided
+//reduce the value to a particular element at each step, with result from previous step
+//logic of for loop
+//Always get a previous value on which the next computation is to be performed (accumulator).
 
-Array.prototype.myMap = function (callback) {
-  var output = [];
-  for (var i = 1; i < this.length; i++) {
-    output.push(callback(this[i], i, this));
+Array.prototype.myReduce = function (callback, initialValue = null) {
+  var accumulator = initialValue;
+  for (var i = 0; i < this.length; i++) {
+    accumulator = callback(accumulator, this[i], i, this);
   }
-
-  return output;
+  return accumulator;
 };
 
 var arr = [1, 2, 3, 4, 5];
 
-var output = arr.myMap((x) => x * 2);
+var output = arr.myReduce(
+  (previousValue, currentValue) => previousValue + currentValue,
+  0
+);
 
 console.log(output);
